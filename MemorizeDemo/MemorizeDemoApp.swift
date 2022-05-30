c504cf2d47eb08c10af30cd7bb292d6fcfd791d9
Artem Paliutin <pal.artem@icloud.com>
2022-05-30 14:49:24 +0300

    Initial Commit

---
+++ MemorizeDemo/MemorizeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeDemoApp.swift
+//  MemorizeDemo
+//
+//  Created by Artem Paliutin on 30/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
